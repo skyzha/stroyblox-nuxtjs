7e5352e19a4c944f2d61b9b7d422115f22ddfbdd
skyzha <rezapermana.aqi@gmail.com>
2019-11-13 17:35:51 +0700

    first complete

---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+export default {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'Storyblok + Nuxt = <3',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'An awesome blog about tech stuff, built with Nuxt and Storyblok'
+      }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https:://fonts.googleapis.com/css?family=Lato:400, 700'
+      }
+    ]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa'
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <section id="posts">
+    <PostPreview
+      v-for="post in posts"
+      :key="post.id"
+      :title="post.title"
+      :excerpt="post.previewText"
+      :thumbnailImage="post.thumbnailUrl"
+      :id="post.id"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Blog/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  },
+  data() {
+    return {
+      posts: [
+        {
+          title: 'A New Beginning',
+          previewText: "This will be awesome, dont't miss it!",
+          thumbnailUrl:
+            'https://images.unsplash.com/photo-1573400780054-c260eb4865b2?ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80',
+          id: 'a-new-beginning'
+        },
+        {
+          title: 'A Second Beginning',
+          previewText: "This will be awesome, dont't miss it!",
+          thumbnailUrl:
+            'https://images.unsplash.com/photo-1569358731637-7a6c762adf28?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
+          id: 'a-second-beginning'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+#posts {
+  padding-top: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 35rem) {
+  #posts {
+    flex-direction: row;
+  }
+}
+</style>
